46d7fece7664508d14e97adcec825651c09dddfb
Jaden Baptista <baptistajaden@gmail.com>
2022-02-10 06:56:37 -0800

    add http post functionality

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,5 +1,7 @@
 
 $(() => {
+
+  // container get requests
   $("#myButton").on("click", () => {
     $.get("http://localhost:8080/api/v1/labor", (data, status) => {
       for (var i = 0; i < data.length; i++) {
@@ -9,5 +11,36 @@ $(() => {
         $("#res").append(container);
       }
     })
+  })
+
+  // container post request
+  $("#postForm").on("submit", (e) => {
+    var name = $("#fname").val();
+    var address = $("#faddress").val();
+    var date = $("#fdate").val();
+    var task = $("#ftask").val();
+    var time = parseFloat($("#ftime").val());
+    var code = parseInt($("#fcode").val());
+
+    var postData = {
+      "name": name,
+      "address": address,
+      "date": date,
+      "task": task,
+      "time": time,
+      "wcCode": code
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/api/v1/labor",
+      data: JSON.stringify(postData),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json"
+    });
+
+    console.log(postData);
+
+    e.preventDefault();
   })
 });
